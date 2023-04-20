8874f7d350429ac0f587035140b8e806ca462ee3
Uros Jankovic <bug24b@gmail.com>
2023-04-20 11:40:54 -0500

    Week 4 code
    - Express http

---
+++ week04/express-example/app.js
@@ -0,0 +1,48 @@
+import express from "express";
+import { engine } from 'express-handlebars';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+});
+
+app.get('/', (req, res) => {
+    res.render('home', {name: 'Bob', time: new Date(), items: ['one', 'two', 'seven']});
+});
+
+app.get('/hello/:name', (req, res) => {
+    console.log(req.params.name);
+    res.send(req.params.name);
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+let todoItems = [];
+
+app.get('/todo', (req, res) => {
+    res.render('todo', {items: todoItems});
+});
+
+app.get('/todo/add/:item', (req, res) => {
+    todoItems.push(req.params.item);
+    res.send('OK!');
+})
+
+app.get('/api', (req, res) => {
+    res.json({
+        path: req.path,
+        domain: req.domain,
+        port: req.port,
+        protocol: req.protocol,
+        query: req.query
+    });
+})
